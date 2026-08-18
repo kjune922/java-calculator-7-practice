156f7cd97e4c487d9e483ce5b839401d06ae1f86
kjune922 <kjune922@naver.com>
2026-08-18 15:53:23 +0900

    feat: [InputValidator] 만약 입력이 비어있다면 0을 반환 구현

--- src/main/java/calculator/validator/InputValidator.java
+++ src/main/java/calculator/validator/InputValidator.java
@@ -9,7 +9,12 @@ public class InputValidator {
      * custom 구분자가 없는 경우 -> 숫자, 쉼표, 콜론 이외의 값 예외처리하고 쉼표(,) 또는 콜론(:)을 구분자로 가짐
      */
 
-    public void validateInput(String input) {
+    public int validateInput(String input) {
+
+        if(input.isBlank()){
+            return 0;
+        }
+
         int n = input.length();
         StringBuilder custom = new StringBuilder();
 
@@ -26,5 +31,6 @@ public class InputValidator {
             custom.append(input.charAt(i));
         }
         System.out.println(custom);
+        return Integer.parseInt(custom.toString());
     }
 }
