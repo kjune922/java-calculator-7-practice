aca662913795d597da658c929a960aa192fb90e5
kjune922 <kjune922@naver.com>
2026-08-21 18:54:43 +0900

    refactor: [README.md, InputParserTest] InputParserTest는 파싱ì  파싱이 ëª 목적인 테스트이기에 Calculator사용을 제외함

--- src/test/java/calculator/parser/InputParserTest.java
+++ src/test/java/calculator/parser/InputParserTest.java
@@ -1,14 +1,12 @@
 package calculator.parser;
 
-import calculator.domain.Calculator;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class InputParserTest {
 
-    private InputParser inputParser = new InputParser();
-    private Calculator calculator = new Calculator();
+    private final InputParser inputParser = new InputParser();
 
     @Test
     void 빈_문자열은_비어있는_숫자목록을_반환() {
@@ -19,13 +17,13 @@ class InputParserTest {
     @Test
     void _12가_입력으로들어오면_3이아닌_12() {
         String input = "12";
-        assertThat(calculator.calculateSum(inputParser.parse(input)))
-                .isEqualTo(12);
+        List<Integer> numbers = inputParser.parse(input);
+        assertThat(numbers).containsExactly(12);
     }
 
     @Test
     void 기본구분자만으로도_숫자를_분리한다() {
-        String input = "1,2,:3";
+        String input = "1,2:3";
         List<Integer> numbers = inputParser.parse(input);
         assertThat(numbers).containsExactly(1,2,3);
     }
@@ -33,22 +31,22 @@ class InputParserTest {
     @Test
     void 기본구분자가_연속이어도_계산가능() {
         String input = "1,,2";
-        assertThat(calculator.calculateSum(inputParser.parse(input)))
-                .isEqualTo(3);
+        List<Integer> numbers = inputParser.parse(input);
+        assertThat(numbers).containsExactly(1,2);
     }
 
     @Test
     void 여러_글자의_커스텀_구분자를_사용할수있다() {
         String input = "//;;\\n1;;2;;3";
-        assertThat(calculator.calculateSum(inputParser.parse(input)))
-                .isEqualTo(6);
+        List<Integer> numbers = inputParser.parse(input);
+        assertThat(numbers).containsExactly(1,2,3);
     }
 
     @Test
     void 커스텀_구분자가_연속이어도_계산가능() {
         String input = "//;\\n1;;2;;;3";
-        assertThat(calculator.calculateSum(inputParser.parse(input)))
-                .isEqualTo(6);
+        List<Integer> numbers = inputParser.parse(input);
+        assertThat(numbers).containsExactly(1,2,3);
     }
 
     @Test
@@ -61,8 +59,8 @@ class InputParserTest {
     @Test
     void 기본_구분자와_커스텀_구분자를_함께_사용가능() {
         String input = "//;\\n1;2,3:4";
-        assertThat(calculator.calculateSum(inputParser.parse(input)))
-                .isEqualTo(10);
+        List<Integer> numbers = inputParser.parse(input);
+        assertThat(numbers).containsExactly(1,2,3,4);
     }
 
     @Test
